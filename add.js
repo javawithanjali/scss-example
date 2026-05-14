b45d9b4b090ffb7ff2333f57a8ad878581ad1b79
javawithanjali <anjali.anu.26@gmail.com>
2026-05-14 22:44:11 +0530

    add js

--- add.js
+++ add.js
@@ -1,19 +1,12 @@
-// Select buttons
 const goodBtn = document.getElementById("goodbtn");
 const badBtn = document.getElementById("badbtn");
 
-// Good experience button click
 goodBtn.addEventListener("click", () => {
     alert("Thanks! 😊 We are happy you had a good experience.");
-    
-    // Optional UI change
     document.body.style.backgroundColor = "#d4edda";
 });
 
-// Bad experience button click
 badBtn.addEventListener("click", () => {
     alert("Sorry! 😒 We will try to improve your experience.");
-    
-    // Optional UI change
     document.body.style.backgroundColor = "#f8d7da";
 });
